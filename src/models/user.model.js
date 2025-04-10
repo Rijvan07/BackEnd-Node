8463dcad80135c799f4e4a8a55956a7bbe3282c3
Rijvan07 <rijvanshaikh888@gmail.com>
2025-04-10 17:43:32 +0530

    add user & video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -92,4 +92,4 @@ userSchema.methods.generateRefreshToken = async function () {
     )
 }
 
-export const User = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema)
